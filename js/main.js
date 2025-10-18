f78bb2c05a5f22777cbb25d746e1375950b18b18
Alvaropana <alvaro.pena@a.vedrunasevillasl.es>
2025-10-18 12:53:14 +0200

    Ejercicio 2 - Fetch y renderizado de los primeros 151 Pokémon

--- js/main.js
+++ js/main.js
@@ -25,6 +25,48 @@ btnElectric.addEventListener('click', () => {
     navMenu.style.backgroundColor = colors.electric;
 });
 // Ejeecicio 2
+const listaPokemon = document.getElementById('listaPokemon');
 
+async function cargarPokemones() {
+  try {
+
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+    const data = await response.json();
+
+    data.results.forEach(async (pokemon) => {
+ 
+      const res = await fetch(pokemon.url);
+      const dataPokemon = await res.json();
+
+      const div = document.createElement('div');
+      div.classList.add('pokemon');
+
+      const nombre = dataPokemon.name;
+      const id = dataPokemon.id.toString().padStart(3, '0'); // Ej: 001
+      const imagen = dataPokemon.sprites.other['official-artwork'].front_default;
+      const tipos = dataPokemon.types.map(t => t.type.name);
+
+      div.innerHTML = `
+        <div class="pokemon-imagen">
+          <img src="${imagen}" alt="${nombre}">
+        </div>
+        <div class="pokemon-info">
+          <div class="nombre-contenedor">
+            <p class="pokemon-id">#${id}</p>
+            <h2 class="pokemon-nombre">${nombre}</h2>
+          </div>
+          <div class="pokemon-tipos">
+            ${tipos.map(tipo => `<p class="tipo ${tipo}">${tipo}</p>`).join('')}
+          </div>
+        </div>
+      `;
+      listaPokemon.appendChild(div);
+    });
+
+  } catch (error) {
+    console.error('Error al cargar los Pokémon:', error);
+  }
+}
+cargarPokemones();
 // Ejercicio 3
 
